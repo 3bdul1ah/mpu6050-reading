9a514c3a115a5a49dda4f32366991046c9416d7e
Abdullah Alshateri <141893142+3bdul1ah@users.noreply.github.com>
2023-12-21 00:42:34 +0800

    Update mpu6050.ino

--- mpu6050.ino
+++ mpu6050.ino
@@ -1,29 +1,26 @@
 #include <Wire.h>
 
-const int MPU_ADDR = 0x68; // I2C address of the MPU-6050
+const int MPU_ADDR = 0x68; 
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(21, 22, 100000); // Initialize I2C communication using SDA and SCL pins (21, 22)
-
-  // Initialize MPU-6050
+  Wire.begin(21, 22, 100000); 
+  
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x6B); // PWR_MGMT_1 register
-  Wire.write(0);    // Set to zero to wake up the MPU-6050
+  Wire.write(0x6B); 
+  Wire.write(0);    
   Wire.endTransmission(true);
 
   Serial.println("Setup complete");
 }
 
 void readMPU6050Data() {
-  // Request data starting from register 0x3B (ACCEL_XOUT_H)
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x3B);
-  Wire.endTransmission(false); // End transmission but maintain connection
-  Wire.requestFrom(MPU_ADDR, 14, true); // Request 14 bytes from MPU-6050
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 14, true); 
 
-  // Read accelerometer and gyroscope data
   AcX = Wire.read() << 8 | Wire.read();
   AcY = Wire.read() << 8 | Wire.read();
   AcZ = Wire.read() << 8 | Wire.read();
@@ -36,7 +33,6 @@ void readMPU6050Data() {
 void loop() {
   readMPU6050Data();
 
-  // Print accelerometer and gyroscope data
   Serial.print("AcX: "); Serial.print(AcX);
   Serial.print(", AcY: "); Serial.print(AcY);
   Serial.print(", AcZ: "); Serial.print(AcZ);
@@ -44,5 +40,5 @@ void loop() {
   Serial.print(", GyY: "); Serial.print(GyY);
   Serial.print(", GyZ: "); Serial.println(GyZ);
 
-  delay(500); // Delay for half a second
+  delay(500); 
 }
